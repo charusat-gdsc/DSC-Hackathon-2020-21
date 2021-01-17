aea84f771b6e7b077085d0c0f16dd9655874540d
Jainam Mehta <jainamm47@gmail.com>
2021-01-17 21:38:22 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path='/' component={Homepage} />
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const Homepage = () => {
+  return <div>HomePage</div>;
+};
+
+export default Homepage;
